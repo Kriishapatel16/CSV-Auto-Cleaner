c9983e3037e14b8d762a2f87858d17096a0ddc43
Kriishapatel16 <krishupatel1609@gmail.com>
2026-09-10 23:06:08 +0530

    Fix python path import in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+# Force Python to look in the root folder for app.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
-# Expose app for Vercel Serverless Function
 app = app
